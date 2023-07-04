c618b743f4c0c5ec142caecbb2bda38894eab422
limsaehyun <sh00710000@gmail.com>
2023-07-04 14:04:33 +0900

    initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,81 @@
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    alias(libs.plugins.com.android.application)
+    alias(libs.plugins.org.jetbrains.kotlin.android)
+    id(libs.plugins.kotlin.ksp.get().pluginId)
+    id("kotlin-kapt")
+//    id("dagger.hilt.android.plugin")
+}
+
+android {
+    namespace = "com.saehyun.ondo100"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "com.saehyun.ondo100"
+        minSdk = 26
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.4.3"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+    packagingOptions {
+        exclude("META-INF/DEPENDENCIES")
+    }
+}
+
+dependencies {
+//    implementation(libs.di.hilt.core)
+//    kapt(libs.di.hilt.compiler)
+
+    implementation(libs.orbit.compose)
+    implementation(libs.orbit.viewmodel)
+
+    implementation(platform(libs.compose.bom))
+    implementation(libs.activity.compose)
+    implementation(libs.compose.ui.coil)
+    implementation(libs.compose.nav)
+    implementation(libs.compose.hilt)
+    implementation(libs.kotlin.collections.immutable)
+
+    implementation(libs.core.ktx)
+    implementation(libs.lifecycle.runtime.ktx)
+    implementation(libs.ui)
+    implementation(libs.ui.graphics)
+    implementation(libs.ui.tooling.preview)
+    implementation(libs.appcompat)
+    implementation(libs.material3)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.test.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+}
